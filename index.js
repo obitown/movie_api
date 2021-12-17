7359805e489374b30f812e60a0fb1e5800498111
obitown <92471740+obitown@users.noreply.github.com>
2021-12-17 11:37:33 -0700

    add movie list

--- index.js
+++ index.js
@@ -2,17 +2,80 @@ const express = require('express'),
     morgan = require('morgan');
 
 const app = express();
+// const bodyParser = require('body-parser'),
+//     methodOverride = require('method-override');
 
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+// app.use(bodyParser.json());
+// app.use(methodOverride());
+
+let movies = [
+    {
+        title:'The Room',
+        year: '2003'
+    },
+    {
+        title: 'Superbad',
+        year: '2007'
+    },
+    {
+        title: 'Shaun of the Dead',
+        year: '2004'
+    },
+    {
+        title: '28 Days Later...',
+        year: '2002'
+    },
+    {
+        title: 'Die Hard',
+        year: '1988'
+    },
+    {
+        title: 'Saving Private Ryan',
+        year: '1998'
+    },
+    {
+        title: 'Star Wars: Episode VI - Return of the Jedi',
+        year: '1983'
+    },
+    {
+        title: 'Pacific Rim: Uprising',
+        year: '2018'
+    },
+    {
+        title: 'Spider-Man',
+        year: '2002'
+    },
+    {
+        title: 'Midsommer',
+        year: '2019'
+    }
+];
+
+//Logging wth Morgan
 app.use(morgan('common'));
 
+//serving Static files in '/public'
+app.use(express.static('public'));
+
+//GET
 app.get('/', (req, res) => {
     res.send('Welcome to my app!');
 });
 
-app.get('/secreturl', (req, res) => {
-    res.send('Secret URl');
+app.get('/movies', (req, res) => {
+    res.json(movies);
 });
 
+//error handaling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something is broken...');
+});
+
+//Listen for requests
 app.listen(8080, () => {
     console.log('app is listening on port 8080');
 });
