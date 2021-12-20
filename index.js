c8809cece4b91e7e16e6a6a2c1c2fcddd568b081
obitown <92471740+obitown@users.noreply.github.com>
2021-12-20 14:03:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,43 +15,103 @@ const app = express();
 let movies = [
     {
         title:'The Room',
-        year: '2003'
+        year: '2003',
+        genre: '',
+        director: {
+            name: '',
+            birth: '',
+            death: ''
+        }
     },
     {
         title: 'Superbad',
-        year: '2007'
+        year: '2007',
+        genre: '',
+        director: {
+            name: '',
+            birth: '',
+            death: ''
+        }
     },
     {
         title: 'Shaun of the Dead',
-        year: '2004'
+        year: '2004',
+        genre: '',
+        director: {
+            name: '',
+            birth: '',
+            death: ''
+        }
     },
     {
         title: '28 Days Later...',
-        year: '2002'
+        year: '2002',
+        genre: '',
+        director: {
+            name: '',
+            birth: '',
+            death: ''
+        }
     },
     {
         title: 'Die Hard',
-        year: '1988'
+        year: '1988',
+        genre: '',
+        director: {
+            name: '',
+            birth: '',
+            death: ''
+        }
     },
     {
         title: 'Saving Private Ryan',
-        year: '1998'
+        year: '1998',
+        genre: '',
+        director: {
+            name: '',
+            birth: '',
+            death: ''
+        }
     },
     {
         title: 'Star Wars: Episode VI - Return of the Jedi',
-        year: '1983'
+        year: '1983',
+        genre: '',
+        director: {
+            name: '',
+            birth: '',
+            death: ''
+        }
     },
     {
         title: 'Pacific Rim: Uprising',
-        year: '2018'
+        year: '2018',
+        genre: '',
+        director: {
+            name: '',
+            birth: '',
+            death: ''
+        }
     },
     {
         title: 'Spider-Man',
-        year: '2002'
+        year: '2002',
+        genre: '',
+        director: {
+            name: '',
+            birth: '',
+            death: ''
+        }
     },
     {
         title: 'Midsommer',
-        year: '2019'
+        year: '2019',
+        genre: '',
+        director: {
+            name: '',
+            birth: '',
+            death: ''
+        }
     }
 ];
 
@@ -66,6 +126,12 @@ app.get('/', (req, res) => {
 
 app.get('/movies', (req, res) => {
     res.json(movies);
+});
+
+app.get('/movies/:title', (req, res) => {
+    res.status(200).json(movies.find((movie) =>{
+        return movie.title === req.params.title
+    }));
 });
 
 //USE
