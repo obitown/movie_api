1f808a94ebbf51c32670ba4a83fe76609d4f6580
obitown <92471740+obitown@users.noreply.github.com>
2022-01-20 09:30:10 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 
