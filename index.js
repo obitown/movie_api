b93333631380617b6e1c1efe743bc0e73564e16d
obitown <92471740+obitown@users.noreply.github.com>
2022-01-21 10:28:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post('/users', [
         return res.status(422).json({ errors: errors.array() });
     }
 
-    let hashedPassword = Users.hashedPassword(req.body.Password);
+    let hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOne({ Username: req.body.Username }) //Search to see if a user with the requested username already exists
         .then((user) => {
             if (user) { //if the user is found, send a response that it already exists
