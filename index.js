8eaa81ecdc91d3cc29d6127e4abb499a2f0deaa0
obitown <92471740+obitown@users.noreply.github.com>
2022-02-23 16:50:44 -0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Users = Models.User;
 
 mongoose.connect('mongodb+srv://obitwon:1234@obiflixdb.ilpfy.mongodb.net/obiFlixDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
-//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 
